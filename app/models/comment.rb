c25a148511ec58ade9d9c4d5eb4003eb0bcc7775
A Stewart <as1481@pc694s.cs.york.ac.uk>
2015-11-27 12:42:20 +0000

    Updated schema to include a deletion flag to flag deleted comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,12 +2,13 @@ class Comment
   include DataMapper::Resource
 
   property :id, Serial
-  property :text, String, required: true, length: 1024
+  property :vagtext, String, required: true, length: 1024
   property :videoid, Integer, required: true
   property :date, DateTime, required: true
+  property :deleted, Boolean, required: false, default: false
   
   has n, :comments
-  belongs_to :comment, :required => false
+  belongs_to :comment, required: false
   belongs_to :user
   
 end
