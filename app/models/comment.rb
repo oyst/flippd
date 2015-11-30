b427e5b6b9f15f42339b8b21d28340d529175589
as1481 <as1481@york.ac.uk>
2015-11-30 16:07:25 +0000

    Added count functions and an edited flag for a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,12 @@ class Comment
   include DataMapper::Resource
 
   property :id, Serial
-  property :vagtext, String, required: true, length: 1024
-  property :videoid, Integer, required: true
+  property :text, String, required: true, length: 1024
+  property :threadid, Integer, required: true
+  property :forumid, Integer, required: true
   property :date, DateTime, required: true
   property :deleted, Boolean, required: false, default: false
+  property :edited, Boolean, required: false, default: false
   
   has n, :comments
   belongs_to :comment, required: false
--- app/routes/commentprovider.rb
+++ app/routes/commentprovider.rb
@@ -5,7 +5,7 @@ class CommentProvider
     @forum_id = forum_id
     @thread_id = thread_id
   end
-
+  
   # Creates a comment in the database
   def create(user_id, message, parent_id)
     if (parent_id == nil)
@@ -14,19 +14,24 @@ class CommentProvider
       Comment.create({ text: message }, { user: user_id }, { date: Time.now }, { threadid: thread_id }, { forumid: forum_id }, { comment: parent_id })
     end
   end
-
+  
   # Retrieves comments from the database for a specified video id
   def get_comments(start, max, parent_id)
-    return Comment.all(:videoid.eql => @thread_id)
+    return Comment.all(:videoid.eql => @thread_id, :order => [ :date.desc ])
   end
-
+  
   # Updates a comment in the database
   def update(comment_id, message)
-    Comment.update(:message => message)
+    Comment.update(:message => message, :edited => true)
   end
-
+  
   # Deletes a comment from the database
   def delete(comment_id)
     Comment.update(:deleted => true)
+  end
+  
+  # Returns the number of replies a comment has
+  def count(parent_id)
+    return Comment.all(:parent_id.eql => @thread_id).count)
   end
 end
