4cb98f1ebd5849027060ee5760bef7eb69274515
Lewis Shaw <lms530@york.ac.uk>
2015-12-02 15:41:10 +0000

    Fixed some indentation

--- app/providers/comment_provider.rb
+++ app/providers/comment_provider.rb
@@ -6,24 +6,24 @@ class CommentProvider
   def self.create(user_id, message, video_id, parent_id)
     Comment.create(:text => message, :user_id => user_id, :date => Time.now, :videoid => video_id, :comment_id => parent_id)
   end
-  
+
   # Retrieves comments from the database for a specified video id
   # Needs to get top level comments with their replies or take a parent id to get chidren
   def self.get_comments(video_id, start, max, parent_id)
     allcomments = Comment.all(:videoid => video_id, :order => [ :date.desc ], :comment_id => nil)
     allcomments[ start, max ]
   end
-  
+
   # Updates a comment in the database
   def update(comment_id, message)
     Comment.update(:message => message, :edited => true)
   end
-  
+
   # Deletes a comment from the database
   def delete(comment_id)
     Comment.update(:deleted => true)
   end
-  
+
   # Returns the number of replies a comment has
   def count(parent_id)
     return Comment.all(:parent_id.eql => @thread_id).count
