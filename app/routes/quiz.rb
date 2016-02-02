a6c5d7674791a7d42e874c73c8025bbc16ebb3e1
Lewis Shaw <lms530@york.ac.uk>
2016-02-02 21:10:36 +0000

    Changed quiz to show selected answers

--- app/routes/quiz.rb
+++ app/routes/quiz.rb
@@ -1,13 +1,5 @@
 class Flippd < Sinatra::Application
   route :get, :post, '/topics/:title/questions' do
-    if params['question']
-      @score = 0
-      params['question'].each do |answer|
-        if answer == 'true'
-          @score += 1
-        end
-      end
-    end
     @topic = nil
     @phases.each do |phase|
       phase['topics'].each do |topic|
@@ -16,6 +8,16 @@ class Flippd < Sinatra::Application
     end
     @questions = @topic['questions']
     pass unless @questions
+    if params['question']
+      @score = 0
+      @questions.each do |question|
+        answerId = params['question']["#{question['id']}"]
+        if answerId == "#{question['correctAnswerId']}" then
+          @score += 1
+        end
+      end
+      @answers = params['question']
+    end
     erb :quiz
   end
 end
