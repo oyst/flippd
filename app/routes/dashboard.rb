c889a103d1b67c7b430ffab983af668ecc5f553c
Lewis Shaw <lms530@york.ac.uk>
2016-02-28 14:38:51 +0000

    Added route for video view form submission

--- app/routes/dashboard.rb
+++ app/routes/dashboard.rb
@@ -2,4 +2,10 @@ class Flippd < Sinatra::Application
   get '/dashboard' do
     erb :dashboard
   end
+  post '/video-view' do
+    redirect to('/auth/new') if @user == nil
+    video_id = params['video_id']
+    View.create(:videoid => video_id, :user => @user, :date => Time.now)
+    redirect back
+  end
 end
