d6d10b8eecf76427a33891183825fe09bd8b415d
Lewis Shaw <lms530@york.ac.uk>
2016-03-03 12:37:13 +0000

    Corrected graph data for the video graph widget

--- app/lib/dashboard/widgets/factories/videoGraphWidget.rb
+++ app/lib/dashboard/widgets/factories/videoGraphWidget.rb
@@ -2,7 +2,7 @@ require_relative '../graph/graphWidget.rb'
 
 class VideoGraphWidget
   def self.create(data)
-    labels = ['October', 'November', 'December', 'January', 'February', 'March', 'April', 'May', 'June']
+    labels = ['October', 'November', 'December', 'January', 'February', 'March', 'April', 'May']
     GraphWidget.new(labels, data, 'Videos Watched Graph')
   end
 end
--- app/lib/videos/videoSummary.rb
+++ app/lib/videos/videoSummary.rb
@@ -1,16 +1,30 @@
+require 'date'
+
 class VideoSummary
   def initialize(moduleData, videoViewProvider)
     @moduleData = moduleData
     @videoProvider = videoViewProvider
   end
+
   def get_videos_by_month(user)
     views = @videoProvider.get_views_by_month(user, @moduleData['startDate'], @moduleData['endDate'])
-    start_date = Time.parse(@moduleData['startDate'])
-    end_date = Time.parse(@moduleData['endDate'])
-    @formatted_views = []
+    start_date = Date.parse(@moduleData['startDate'])
+    end_date = Date.parse(@moduleData['endDate'])
+    date_range = start_date..end_date
+    date_months = date_range.map {|d| Date.new(d.year, d.month, 1) }.uniq
+    formatted_views = {}
     views.each do |view|
-      @formatted_views.push(view.total_views)
+      formatted_views[view.month] = view.total_views
     end
-    return @formatted_views
+    @views_months = []
+    date_months.each do |month|
+      if formatted_views[month.month]
+        views =  formatted_views[month.month]
+      else
+        views = 0
+      end
+      @views_months.push(views)
+    end
+    return @views_months
   end
 end
