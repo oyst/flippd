c417d159b2b0678edf0e4fe82516d9849e96ab9b
Alex Oyston <alexoyston93@gmail.com>
2016-03-03 23:01:30 +0000

    Simplify and comment the month list

--- app/lib/videos/videoSummary.rb
+++ app/lib/videos/videoSummary.rb
@@ -8,18 +8,20 @@ class VideoSummary
 
   def get_videos_by_month(user)
     views = @video_provider.get_views_by_month(user, @module_provider.get_start_date, @module_provider.get_end_date)
+    # Create a list of months from start_date to end_date
+    # Assumes that there will be only one occurance of each month
     start_date = Date.parse(@module_provider.get_start_date)
     end_date = Date.parse(@module_provider.get_end_date)
     date_range = start_date..end_date
-    date_months = date_range.map {|d| Date.new(d.year, d.month, 1) }.uniq
+    date_months = date_range.map {|d| d.month }.uniq
     formatted_views = {}
     views.each do |view|
       formatted_views[view.month] = view.total_views
     end
     @views_months = []
     date_months.each do |month|
-      if formatted_views[month.month]
-        views =  formatted_views[month.month]
+      if formatted_views[month]
+        views =  formatted_views[month]
       else
         views = 0
       end
