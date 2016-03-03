78e9ae7d56fefbc73d338e606863c55e0a8d689c
Lewis Shaw <lms530@york.ac.uk>
2016-03-03 22:33:14 +0000

    Changed camelCase to snake_case

--- app/lib/videos/videoSummary.rb
+++ app/lib/videos/videoSummary.rb
@@ -1,15 +1,15 @@
 require 'date'
 
 class VideoSummary
-  def initialize(moduleProvider, videoViewProvider)
-    @moduleProvider = moduleProvider
-    @videoProvider = videoViewProvider
+  def initialize(module_provider, video_view_provider)
+    @module_provider = module_provider
+    @video_provider = video_view_provider
   end
 
   def get_videos_by_month(user)
-    views = @videoProvider.get_views_by_month(user, @moduleProvider.get_start_date, @moduleProvider.get_end_date)
-    start_date = Date.parse(@moduleProvider.get_start_date)
-    end_date = Date.parse(@moduleProvider.get_end_date)
+    views = @video_provider.get_views_by_month(user, @module_provider.get_start_date, @module_provider.get_end_date)
+    start_date = Date.parse(@module_provider.get_start_date)
+    end_date = Date.parse(@module_provider.get_end_date)
     date_range = start_date..end_date
     date_months = date_range.map {|d| Date.new(d.year, d.month, 1) }.uniq
     formatted_views = {}
@@ -29,16 +29,16 @@ class VideoSummary
   end
 
   def get_videos_viewed(user)
-    videosViewed = []
-    @moduleProvider.get_all_videos.each do |video|
-      videoView = @videoProvider.get_view(user, video['slug'])
-      if videoView
+    videos_viewed = []
+    @module_provider.get_all_videos.each do |video|
+      video_view = @video_provider.get_view(user, video['slug'])
+      if video_view
         video['viewed'] = 'yes'
       else
         video['viewed'] = 'no'
       end
-      videosViewed.push(video)
+      videos_viewed.push(video)
     end
-    return videosViewed
+    return videos_viewed
   end
 end
