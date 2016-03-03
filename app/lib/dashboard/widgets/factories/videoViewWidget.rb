b0998e38ebe19bcdcebbb9d5a0c63d1d4c12202c
Lewis Shaw <lms530@york.ac.uk>
2016-03-03 20:17:33 +0000

    Added video view widget factory

---
+++ app/lib/dashboard/widgets/factories/videoViewWidget.rb
@@ -0,0 +1,10 @@
+require_relative '../table/tableWidget.rb'
+require_relative '../table/tableColumn.rb'
+class VideoViewWidget
+  def self.create(data)
+    video  = TableColumn.new('Video', 'title')
+    viewed = TableColumn.new('Viewed', 'viewed')
+    columns = [video, viewed]
+    TableWidget.new(columns, data, 'My Videos')
+  end
+end
