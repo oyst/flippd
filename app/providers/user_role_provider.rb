53831b4480a065c0039aff11d5105c2b907713e9
Lewis Shaw <lms530@york.ac.uk>
2016-03-06 14:48:16 +0000

    Changed role check to make use of include?

--- app/providers/user_role_provider.rb
+++ app/providers/user_role_provider.rb
@@ -4,12 +4,8 @@ class UserRoleProvider
   end
 
   def has_role(user, role)
-    has_role = false;
     users = @module_provider.get_users_by_role(role)
-    users.each do |email|
-      has_role = true if email == user.email
-    end
-    has_role
+    users.include?(user.email)
   end
 
 end
