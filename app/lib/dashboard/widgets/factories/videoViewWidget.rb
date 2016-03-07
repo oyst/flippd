0f4af3127d095551e36e5b8a5218c58ca9738c85
Lewis Shaw <lms530@york.ac.uk>
2016-03-07 16:47:34 +0000

    Improved lambda for handling bool

--- app/lib/dashboard/widgets/factories/videoViewWidget.rb
+++ app/lib/dashboard/widgets/factories/videoViewWidget.rb
@@ -3,8 +3,7 @@ require_relative '../table/tableColumn.rb'
 class VideoViewWidget
   def self.create(data)
     video  = TableColumn.new('Video', 'title')
-    bool_to_answer = lambda { |bool| if bool then 'yes' else 'no' end }
-    viewed = TableColumn.new('Viewed', 'viewed', bool_to_answer)
+    viewed = TableColumn.new('Viewed', 'viewed', lambda { |bool| bool ? 'yes' : 'no' })
     columns = [video, viewed]
     TableWidget.new(columns, data, 'My Videos')
   end
