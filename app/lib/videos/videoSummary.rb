3fb7a24ed14482955e577966afd078b7d88e7340
Lewis Shaw <lms530@york.ac.uk>
2016-03-08 14:30:59 +0000

    Simplified function for getting views by month

--- app/lib/videos/videoSummary.rb
+++ app/lib/videos/videoSummary.rb
@@ -7,27 +7,17 @@ class VideoSummary
   end
 
   def get_videos_by_month(user)
-    views = @video_provider.get_views_by_month(user, @module_provider.get_start_date, @module_provider.get_end_date)
     # Create a list of months from start_date to end_date
     # Assumes that there will be only one occurance of each month
     start_date = Date.parse(@module_provider.get_start_date)
     end_date = Date.parse(@module_provider.get_end_date)
     date_range = start_date..end_date
     date_months = date_range.map {|d| d.month }.uniq
-    formatted_views = {}
-    views.each do |view|
-      formatted_views[view.month] = view.total_views
-    end
     @views_months = {}
     date_months.each do |month|
-      if formatted_views[month]
-        views =  formatted_views[month]
-      else
-        views = 0
-      end
-      @views_months[month] = views
+      @views_months[month] = @video_provider.get_views_by_month(user, month, start_date, end_date) or 0
     end
-    return @views_months
+    @views_months
   end
 
   def get_videos_viewed(user)
