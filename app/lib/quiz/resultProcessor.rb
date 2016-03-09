77ab95d3f37b890cd457b646009427dc1388bcc0
Lewis Shaw <lms530@york.ac.uk>
2016-03-09 23:20:28 +0000

    Fixed indentation

--- app/lib/quiz/resultProcessor.rb
+++ app/lib/quiz/resultProcessor.rb
@@ -1,8 +1,8 @@
 class QuizResultProcessor
 
   def get_result(questions, submitted_questions)
-      correct_questions = questions.select { |question| submitted_questions[question['id'].to_s] == question['correctAnswerId'].to_s }
-      correct_questions = correct_questions.map { |question| question['id'] }
-      QuizResult.new(correct_questions)
+    correct_questions = questions.select { |question| submitted_questions[question['id'].to_s] == question['correctAnswerId'].to_s }
+    correct_questions = correct_questions.map { |question| question['id'] }
+    QuizResult.new(correct_questions)
   end
 end
